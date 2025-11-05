759f55c4377dd78b2ba9f7af9c5add5ece2da53d
Denis Egorenko <degorenko@mirantis.com>
2025-11-05 15:34:51 +0400

    Add CI workflow for building docker images and helm
    Add workflows for:
    * helm chart checks (lint and package);
    * go binary build;
    * docker images build;
    
    Signed-off-by: Denis Egorenko degorenko@mirantis.com
    (cherry picked from commit 2bf08bd77ec6ed127218015c6d4bb5bf9af339f4)

--- build/scripts/get_version.sh
+++ build/scripts/get_version.sh
@@ -8,58 +8,28 @@ function _git {
     ${GIT_CMD} "$@"
 }
 
-function main {
-    arg=$1
-    if [[ "${arg}" == "app" || "${arg}" == "chart" ]]; then
-        get_version_for $arg
-    else
-        echo "unknown '${arg}' type requested to show version (possible values: app, chart)"
-        exit 1
-    fi
-}
-
-function get_version_for {
-    kind=$1
+function get_version {
+    main_version=$1
+    build_mode=$2
     version=""
-    cur_commit="$(_git rev-parse --short=8 HEAD | cut -b 1-8)"
-    cur_branch="$(_git branch --show-current)"
-    # (degorenko): set branch if we are on gerrit merge event
-    # keep for backward compatibility for CI
-    if [[ "${GERRIT_BRANCH}" != "" && "${GERRIT_EVENT_TYPE}" == "change-merged" ]]; then
-        cur_branch=${GERRIT_BRANCH}
-    fi
-    # if we are on release branch - release versions should be prepared
-    if [[ "${cur_branch}" =~ ^release- ]]; then
-        last_tag="$(_git describe --abbrev=0 --tags)"
-        commits_since_tag="$(_git rev-list --count "${last_tag}"..HEAD)"
-        if [[ "${commits_since_tag}" != "0" ]]; then
-            version=${echo $last_tag | sed -E "s/[0-9]+$/${commits_since_tag}/"}
-        else
-            version=${last_tag}
-        fi
+    cur_commit="$(_git rev-parse --short=8 HEAD)"
+    if [[ "${build_mode}" == "dev" ]]; then
+        version="${main_version}-dev-${cur_commit}"
     else
-        if [[ "${kind}" == "app" ]]; then
-            if [[ "${cur_branch}" != "" ]]; then
-                version="${cur_branch}-${cur_commit}"
+        last_tag="$(_git describe --abbrev=0 --tags --always)"
+        # check we are good for release
+        if [[ "${last_tag}" == "${main_version}" ]]; then
+            commits_since_tag="$(_git rev-list --count "${last_tag}"..HEAD)"
+            if [[ "${commits_since_tag}" != "0" ]]; then
+                version="${echo $last_tag | sed -E "s/[0-9]+$/${commits_since_tag}/"}"
             else
-                version="dev-${cur_commit}"
+                version="${last_tag}"
             fi
         else
-            chart_file="charts/pelagia-ceph/Chart.yaml"
-            if [[ -f ${chart_file} ]]; then
-                current_semantic_version=$(grep -E "^version:" ${chart_file} | cut -d: -f2 | tr -d ' ')
-                if [[ "${cur_branch}" != "" ]]; then
-                    version="${current_semantic_version}-${cur_branch//\//-}-${cur_commit}"
-                else
-                    version="${current_semantic_version}-dev-${cur_commit}"
-                fi
-            else
-                echo "file '$(pwd)/${chart_file}' is not found"
-                exit 1
-            fi
+            version="${main_version}-custom-${cur_commit}"
         fi
     fi
     echo ${version}
 }
 
-main "$@"
+get_version "$@"
